55ce6bbe76e8c1aee029d38d9d91945ba5e7795d
Mike Tatsuo <michael@ibureau.com.br>
2020-02-13 03:25:46 -0300

    partially working with firebase messaging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import * as firebase from 'nativescript-plugin-firebase';
 
 @Component({
-    selector: "ns-app",
-    templateUrl: "./app.component.html"
+  selector: "ns-app",
+  templateUrl: "./app.component.html"
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    firebase.init({
+      showNotifications: true,
+      showNotificationsWhenInForeground: true,
+      onPushTokenReceivedCallback: (token) => {
+        console.log("Firebase plugin received a push token: " + token);
+      },
+      onMessageReceivedCallback: (message: firebase.Message) => {
+        console.log("Push message received: " + JSON.stringify(message));
+      }
+    }).then(() => console.log("Registered for push"))
+      .catch(error => console.log("Push message erro: ", { error }));
+  }
+}
