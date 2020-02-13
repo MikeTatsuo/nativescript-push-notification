de452366e8c6316778a206eed11cdc1143bc0a86
Mike Tatsuo <michael@ibureau.com.br>
2020-02-13 04:08:38 -0300

    partially working with external push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { messaging, Message } from "nativescript-plugin-firebase/messaging";
 
-import * as firebase from 'nativescript-plugin-firebase';
+require("nativescript-plugin-firebase");
 
 @Component({
   selector: "ns-app",
@@ -9,16 +10,30 @@ import * as firebase from 'nativescript-plugin-firebase';
 export class AppComponent implements OnInit {
 
   ngOnInit() {
-    firebase.init({
-      showNotifications: true,
-      showNotificationsWhenInForeground: true,
-      onPushTokenReceivedCallback: (token) => {
+    console.log(`Notifications enabled? ${messaging.areNotificationsEnabled()}`);
+
+    messaging.registerForPushNotifications({
+      onPushTokenReceivedCallback: (token: string): void => {
         console.log("Firebase plugin received a push token: " + token);
       },
-      onMessageReceivedCallback: (message: firebase.Message) => {
-        console.log("Push message received: " + JSON.stringify(message));
-      }
-    }).then(() => console.log("Registered for push"))
-      .catch(error => console.log("Push message erro: ", { error }));
+
+      onMessageReceivedCallback: (message: Message) => {
+        console.log("Push message received: ", JSON.stringify(message));
+      },
+
+      // Whether you want this plugin to automatically display the notifications or just notify the callback. Currently used on iOS only. Default true.
+      showNotifications: true,
+
+      // Whether you want this plugin to always handle the notifications when the app is in foreground. Currently used on iOS only. Default false.
+      showNotificationsWhenInForeground: true
+    }).then(() => console.log("Registered for push"));
+
+    messaging.getCurrentPushToken()
+      .then(token => console.log(`Current push token: ${token}`));
+    
+    messaging.addOnMessageReceivedCallback(message => {
+      console.log('addOnMessageReceived: ', JSON.stringify(message));
+      
+    })
   }
 }
